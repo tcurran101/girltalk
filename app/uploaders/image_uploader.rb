f7933408c9261c6a365d2ff4cfe8fc60b7ddda8c
Tiffany Curran <tcurran101@gmail.com>
2020-02-01 15:49:41 +0000

    updated photo grid and image reize

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [200, 300]
+  process resize_to_fill: [200, 250]
   #
   # def scale(width, height)
   #   # do something
