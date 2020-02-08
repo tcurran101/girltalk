63c70edc8db52cdbd48dd3bd8d4be7eda28a7b65
Tiffany Curran <tcurran101@gmail.com>
2020-02-08 20:21:31 +0000

    fixed factory-devise rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,6 +40,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.include Devise::TestHelpers, type: :controller
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -56,6 +57,7 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
+  config.include FactoryBot::Syntax::Methods
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
