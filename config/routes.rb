f7aa226f23b345d69d600b9cd53c0529f1701866
Tiffany Curran <tcurran101@gmail.com>
2020-02-09 13:15:17 +0000

    changed route after signin

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   get 'home', to: 'static_pages#home'
 
+  namespace :user do
+    root :to => "static_pages#home"
+  end
+
   resources :photos
   resources :users, only: :show
   resources :blogs
